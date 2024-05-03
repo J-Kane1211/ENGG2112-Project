b0296716fb2c3473952dadeef5bb997a7a26c279
Daniel Geng <gengruize@gmail.com>
2024-05-03 14:43:39 +1000

    changed one-hot encoding

--- validate.py
+++ validate.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.metrics import roc_curve, auc
 from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import OneHotEncoder
 
 #calculatees the mean roc
 def validate_single_classifier(
@@ -29,29 +30,23 @@ def validate_single_classifier(
 
 def convert_features(df):
     res = pd.DataFrame()
+    enc = OneHotEncoder(drop='first')
     for label in df.columns:
         if df[label].dtype not in ('string', 'category', 'object'):
             res[label] = df[label] #normal add
         else:
             #use one hot encoding for categories and the like
             #get k-1 labels
+            new = pd.DataFrame(enc.fit_transform(df[[label]]).toarray(), columns = enc.get_feature_names_out([label]))
             res = pd.concat([
                 res,
-                pd.get_dummies(df[label], prefix=label, drop_first=True)],
+                new],
                 axis=1
             )
     return res
 
-
-from sklearn.neighbors import KNeighborsClassifier
-
-df = pd.read_csv("heart.csv")
-features = df[["Age", "Sex", "ChestPainType", "ExerciseAngina"]]
-features = convert_features(features)
-
-
-predict = df["HeartDisease"]
-validate_single_classifier(features.to_numpy(), predict.to_numpy(), KNeighborsClassifier())
+if __name__ == "__main__":
+    pass
 
 
 
