79831ebe82e9c1b88c8951e293504495ae39ce19
alfieeagle <126121089+alfieeagle@users.noreply.github.com>
2024-05-24 14:41:05 +1000

    Update ThresholdFunction with new catagories

--- ThresholdFunction.py
+++ ThresholdFunction.py
@@ -1,5 +1,10 @@
 def threshold(modelOutput):
-    if modelOutput > 0.5:
-        return 1
-    else:
-        return 0
+    if modelOutput < 0.1:
+        print("Low risk. No need to address.")
+    else if (modelOutput >= 0.1 & modelOutput < 0.35):
+        print("Medium risk. Address at next GP appointment.")
+    else if (modelOutput >= 0.35 & modelOutput < 0.75):
+        print("High risk. Book appointment within the next 2 months.")
+    else
+        print("Very high risk. Book appointment ASAP.")
+        
